3ff4fc45476d6c518974accebce5eed6b2a559f9
Gintas <pikolas666@gmail.com>
2024-05-07 13:16:47 +0300

    added mobile-nav

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 	anchor.addEventListener("click", function (e) {
 		e.preventDefault();
+		console.log("boom");
 
 		document.querySelector(this.getAttribute("href")).scrollIntoView({
 			behavior: "smooth",
 		});
 	});
+});
+
+const mobileNav = document.querySelector(".mobile-nav");
+const burger = document.querySelector(".burger-container");
+burger.addEventListener("click", () => {
+	burger.classList.toggle("open");
+	mobileNav.classList.toggle("active");
 });
